bee776a91b9821a735c2616a379a5701c640d2ae
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-05-01 21:06:07 +0530

    Added Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
+import CoinTable from "./components/CoinTable/CoinTAble";
 function App() {
   return (
-    <>Hello</>
-  )
+    <div className="min-h-screen bg-base-100 text-base-content p-10">
+      <h1 className="text-3xl font-bold">Crypto Tracker</h1>
+      <main>
+        <CoinTable />
+      </main>
+    </div>
+  );
 }
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["light", "dark", "night", "dracula"],
   },
-  plugins: [require("daisyui")],
-}
+};
